e8b26ef76e171a390a00fd94280dc38b0604b438
Jim Knight <jim.knight@lavatech.com>
2012-02-12 19:07:51 -0500

    added root url to logo link

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -37,19 +37,12 @@ class DocumentsController < ApplicationController
     @document = Document.find(params[:id])
   end
 
-  # POST /documents
-  # POST /documents.json
   def create
     @document = Document.new(params[:document])
-
-    respond_to do |format|
-      if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render json: @document, status: :created, location: @document }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    if @document.save
+      redirect_to documents_path, :notice => "Document was successfully created"
+    else
+      render "new"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Documenterist::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'documents#index'
 
   # See how all your routes lay out with "rake routes"
 
