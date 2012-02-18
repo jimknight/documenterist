5638c8981a7559435a403a10d75ed2a31788d2e6
Jim Knight <jim.knight@lavatech.com>
2012-02-18 09:20:28 -0500

    comments: got a tree working in general indented

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,15 +4,9 @@ class DocumentsController < ApplicationController
     @documents = Document.all
   end
 
-  # GET /documents/1
-  # GET /documents/1.json
   def show
     @document = Document.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @document }
-    end
+   @comments = @document.comments
   end
 
   # GET /documents/new
